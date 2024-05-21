e7f90913dd9a71543f60d877bb8c08eacb72cf17
Kotilikit <kotilikit@yandex.ru>
2024-05-21 22:14:17 +0300

    ДЗ 4

--- skypro-music/src/app/page.tsx
+++ skypro-music/src/app/page.tsx
@@ -1,38 +1,26 @@
-"use client";
-
 import styles from "./page.module.css";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Nav from "@/components/Nav/Nav";
 import Player from "@/components/Player/Player";
 import Playlist from "@/components/Playlist/Playlist";
 import Search from "@/components/Search/Search";
-import { useState } from "react";
-import { trackType } from "@/types";
 
 export default function Home() {
-  const [track, setTrack] = useState<trackType | null>(null);
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <main className={styles.main}>
           <Nav />
           <div className={styles.mainCenterblock}>
-            <Search/>
-            <Playlist 
-            setTrack={setTrack}
-            setIsPlaying={setIsPlaying}
+            <Search />
+            <Playlist
             />
           </div>
           <Sidebar />
         </main>
-        {track && (
         <Player 
-            track={track}
-            isPlaying={isPlaying}
-            setIsPlaying={setIsPlaying}
-            />
-        )}
+        />
         <footer className={styles.footer} />
       </div>
     </div>
--- skypro-music/src/components/Track/Track.tsx
+++ skypro-music/src/components/Track/Track.tsx
@@ -1,18 +1,37 @@
+"use client";
+
+import { trackType } from "@/types";
 import styles from "./Track.module.css";
 import classNames from 'classnames';
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { setCurrentTrack, toggleIsPlaying } from "@/store/features/playlistSlice";
 type TrackType = {
-  name: string;
-  author: string;
-  album: string;
-  onClick: () => void;
+  track: trackType;
+  tracksData: trackType[];
 };
 
-export default function Track({ name, author, album, onClick }: TrackType) {
+export default function Track({ track, tracksData }: TrackType) {
+  const currentTrack = useAppSelector((state) => state.playlist.currentTrack);
+  const { name, author, album, duration_in_seconds, id } = track;
+  const { isPlaying } = useAppSelector((store) => store.playlist);
+
+  const dispatch = useAppDispatch();
+  const handleTrackClick = () => {
+    dispatch(setCurrentTrack({ track, tracksData }));
+    dispatch(toggleIsPlaying(true));
+  };
   return (
-    <div onClick={onClick} className={styles.playlistItem}>
+    <div onClick={handleTrackClick} className={styles.playlistItem}>
       <div className={classNames(styles.playlistTrack)}>
         <div className={styles.trackTitle}>
           <div className={styles.trackTitleImage}>
+          {currentTrack?.id === track.id && (
+              <div
+                className={`${
+                  isPlaying ? styles.playingDot : styles.stoppedDot
+                }`}
+              ></div>
+            )}
             <svg className={styles.trackTitleSvg}>
               <use xlinkHref="img/icon/sprite.svg#icon-note" />
             </svg>
